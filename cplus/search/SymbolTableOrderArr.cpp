054b2acd60debe3d62a2674c6e6a375a4830b032
baskar Shanmugam <hibasky@gmail.com>
2017-12-29 09:49:37 +0530

    Added Ordered symbol table implementation using Arrays

---
+++ cplus/search/SymbolTableOrderArr.cpp
@@ -0,0 +1,115 @@
+//============================================================================
+// Name        : SymbolTableOrderArr.cpp
+// Author      : Baskar Shanmugam
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Ordered Symbol table using dynamic arrays
+//============================================================================
+
+
+#include "SymbolTableOrderArr.h"
+
+template<typename Key, typename Value>
+size_t SymbolTableOrderArr<Key, Value>::rank (Key k) {
+    size_t h_pos = count;
+    size_t l_pos = 0;
+    size_t m_pos = 0;
+
+    if (!count) {
+        return l_pos;
+    }
+
+    while ((l_pos <= h_pos) && (l_pos < count)) {
+        m_pos = (l_pos + h_pos)/2;
+        if (key[m_pos] == k) {
+            return m_pos;
+        } else if (key[m_pos] < k) {
+            l_pos = m_pos + 1;
+        } else {
+            h_pos = m_pos - 1;
+        }
+    }
+
+    return l_pos;
+}
+
+template<typename Key, typename Value>
+void SymbolTableOrderArr<Key, Value>::put (Key k, Value v) {
+    if (count+1 == capacity) {
+        resize(capacity*2);
+    }
+
+    size_t pos = rank(k);
+    size_t mv_pos = count;
+    while(pos != mv_pos) {
+        key[mv_pos] = key[mv_pos-1];
+        val[mv_pos] = val[mv_pos-1];
+        mv_pos--;
+    }
+
+    key[pos] = k;
+    val[pos] = v;
+
+    count++;
+}
+
+template<typename Key, typename Value>
+Value SymbolTableOrderArr<Key, Value>::get(Key k) {
+    size_t pos = rank(k);
+    if (key[pos] == k) {
+        return val[pos];
+    }
+    return NULL;
+}
+
+template<typename Key, typename Value>
+void SymbolTableOrderArr<Key, Value>::deleteEntry(Key k) {
+    if (contains(k)) {
+        size_t mv_pos = rank(k);
+
+        while(count-1 > mv_pos) {
+            key[mv_pos] = key[mv_pos+1];
+            val[mv_pos] = val[mv_pos+1];
+            mv_pos++;
+        }
+
+        key[mv_pos] = NULL;
+        //val[mv_pos] = NULL;
+        count--;
+    }
+
+}
+
+template<typename Key, typename Value>
+bool SymbolTableOrderArr<Key, Value>::contains(Key k) {
+    size_t pos = rank(k);
+    return (key[pos] == k? true: false);
+}
+
+
+int main() {
+	std::cout<< "Next Step! All the best"<< std::endl;
+	SymbolTableOrderArr<int,int> st(10);
+	std::cout << st.size() << " " << st.isEmpty() << std::endl;
+	st.put(10, 1000);
+	st.put(20, 20000);
+	std::cout << "Key 10: Value: " << st.get(10) << std::endl;
+	std::cout << "Key 20: Value: " << st.get(20) << std::endl;
+	std::cout << st.size() << " " << st.isEmpty()<< std::endl;
+	st.deleteEntry(20);
+	std::cout << st.size() << " " << st.isEmpty()<< std::endl;
+	st.deleteEntry(10);
+	std::cout << st.size() << " " << st.isEmpty()<< std::endl;
+	SymbolTableOrderArr<int, std::string> st1(20);
+	std::cout << st.size() << " " << st.isEmpty() << std::endl;
+    st1.put(10, "5000");
+    st1.put(20, "7000");
+    std::cout << "Key 10: Value: " << st1.get(10) << std::endl;
+    std::cout << "Key 20: Value: " << st1.get(20) << std::endl;
+    std::cout << st1.size() << " " << st1.isEmpty()<< std::endl;
+    st1.deleteEntry(20);
+    std::cout << st1.size() << " " << st1.isEmpty()<< std::endl;
+    st1.deleteEntry(10);
+    std::cout << st1.size() << " " << st1.isEmpty()<< std::endl;
+
+}
---
+++ cplus/search/SymbolTableOrderArr.h
@@ -0,0 +1,122 @@
+//============================================================================
+// Name        : SymbolTableOrderArr.h
+// Author      : Baskar Shanmugam
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Ordered Symbol Table using Arrays
+//============================================================================
+#ifndef SYMBOLTABLEORDERARR_H_
+#define SYMBOLTABLEORDERARR_H_
+#include <iostream>
+#include <vector>
+
+template<typename Key, typename Value>
+class SymbolTableOrderArr {
+public:
+	//Default Ctor
+    SymbolTableOrderArr(size_t cap){
+        capacity = cap;
+        key = new Key[capacity];
+        val = new Value[capacity];
+        for (size_t i = 0; i < capacity; i++) {
+            key[i] = NULL;
+            //val[i] = 0;
+        }
+        count = 0;
+    }
+    // Dtor
+    ~SymbolTableOrderArr() {
+        delete key;
+        delete val;
+    }
+
+    // put key-value pair into the table
+    // (remove key from table if value is null)
+    void put (Key key, Value val);
+
+    // value paired with key
+    // (null if key is absent)
+    Value get(Key key);
+
+    // remove key(and its value) from table
+    void deleteEntry(Key key);
+
+    // is there a value paired with key?
+    bool contains(Key key);
+
+    // is the table empty?
+    bool isEmpty() {
+        return ((count == 0)?true:false);
+    }
+
+    // number of key-value pairs in the table
+    size_t size() {
+        return count;
+    }
+
+    // smallest key
+    Key min() {
+        if (count) {
+            return key[0];
+        }
+        return NULL;
+    }
+
+    // largest key
+    Key max() {
+        if (count) {
+            return key[count-1];
+        }
+        return NULL;
+    }
+
+    // largest key less than or equal to key
+    Key floor(Key key);
+
+    // smallest key greater than or equal to key
+    Key ceiling(Key key);
+
+    // number of keys less than key
+    size_t rank(Key key);
+
+    // key of rank k
+    Key select(int k);
+
+    // delete smallest key
+    void deleteMin() {
+        deleteEntry(min());
+    }
+
+    // delete largest key
+    void deleteMax() {
+        deleteEntry(max());
+    }
+
+    // number of keys in [lo..hi]
+    int size(Key lo, Key hi);
+
+
+private:
+    Key* key;
+    Value* val;
+    size_t count;
+    size_t capacity;
+    Value nullitem;
+
+    void resize(size_t cap) {
+        Key* key_tmp = new Key[cap];
+        Value* val_tmp  = new Value[cap];
+        for (size_t itr = 0; itr < count; itr++) {
+            key_tmp[itr] = key[itr];
+            val_tmp[itr] = val[itr];
+        }
+        delete key;
+        delete val;
+        key = key_tmp;
+        val = val_tmp;
+        capacity = cap;
+    }
+
+
+};
+#endif
